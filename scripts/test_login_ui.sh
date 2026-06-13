c9420f6c1bfb0276db57c34069ab52ad7975a447
naomisphere <parou.sia@tuta.io>
2026-06-13 11:34:19 -0400

    fix test_login_ui.sh to use AUTHBUNDLE
    script previously used hardcoded path BASEDIR/../AuthorizationBundle which wasn't ideal at all.

--- scripts/test_login_ui.sh
+++ scripts/test_login_ui.sh
@@ -4,17 +4,23 @@ set -e
 BINARY=/tmp/UIAuthBundleTest
 #BASEDIR="${0:a:h}"
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ -z "$AUTHBUNDLE" ]; then
+    echo "AUTHBUNDLE must be set" >&2
+    exit 1
+fi
 RESOURCES_DIR="$(dirname "$BINARY")/Resources"
 
 echo "compiling present login UI..."
-swiftc ${BASEDIR}/../AuthorizationBundle/core/LoginUI.swift ${BASEDIR}/test_login_ui.swift \
-    ${BASEDIR}/../AuthorizationBundle/core/SettingsManager.swift \
-    ${BASEDIR}/../AuthorizationBundle/core/BundleLog.swift \
+swiftc "$AUTHBUNDLE/core/LoginUI.swift" "$BASEDIR/test_login_ui.swift" \
+    "$AUTHBUNDLE/core/SettingsManager.swift" \
+    "$AUTHBUNDLE/core/BundleLog.swift" \
     -framework OpenDirectory \
     -o "$BINARY"
 
 mkdir -p "$RESOURCES_DIR"
-cp ${BASEDIR}/../AuthorizationBundle/Resources/Comfortaa-*.ttf "$RESOURCES_DIR/"
+find "$AUTHBUNDLE/Resources" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' resource; do
+    cp "$resource" "$RESOURCES_DIR"
+done
 
 echo "done, running login UI"
 "$BINARY"
